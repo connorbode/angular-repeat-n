8862eb92692e65f666586493a689cfeeb569372c
Connor Bode <connor.bode@gmail.com>
2014-08-09 16:28:39 -0400

    can now increase values

--- src/angular-repeat-n.js
+++ src/angular-repeat-n.js
@@ -18,18 +18,27 @@ angular.module('angular-repeat-n', [])
 
         scope.$watch('repeat', function (newValue, oldValue) {
 
-          var num = parseInt(newValue);
+          var newInt = parseInt(newValue)
+            , oldInt = parseInt(oldValue)
+            , bothValues = ! isNaN(newInt) && ! isNaN(oldInt)
+            , childScope;
 
-          if (isNaN(num)) {
+          // decrease number of repeated elements
+          if (isNaN(newInt) || (bothValues && newInt < oldInt)) {
             scope.last = scope.elems[0];
-            for (i = 1; i < scope.elems.length; i += 1) {
+            for (i = scope.elems.length - 1; i > 1; i -= 1) {
               scope.parentElem.remove(scope.elems[i]);
             }
-          } else {
-            for (i = 0; i < newValue; i += 1) {
-              repeatScope = scope.$new();
-              repeatScope.$index = i;
-              $transclude(repeatScope, function (clone) {
+          } 
+
+          // increase number of repeated elements
+          else {
+            i = scope.elems.length - 1;
+
+            for (i; i < newInt; i += 1) {
+              childScope = scope.$new();
+              childScope.$index = i;
+              $transclude(childScope, function (clone) {
                 scope.last.after(clone);
                 scope.last = clone;
                 scope.elems.push(clone);
